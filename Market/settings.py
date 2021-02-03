c9508dad14658e80980038d851e5ea59d8c529b9
Cristhian Mauricio <cmyarap@gmail.com>
2021-02-02 19:16:23 -0500

    Se corrgie default base de datos

--- Market/settings.py
+++ Market/settings.py
@@ -118,7 +118,7 @@ import dj_database_url
 from decouple import config
 
 DATABASES = {
-    'default':fj_database_url.config(
+    'default':dj_database_url.config(
         default = config('DATABASE_URL'),
     )
 }
