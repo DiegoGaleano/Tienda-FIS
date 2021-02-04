ec4ed043e3f563297964ea8892360bfa5859a4fa
Cristhian Mauricio <cmyarap@gmail.com>
2021-02-03 19:44:57 -0500

    Prueba de despliegue

--- Market/wsgi.py
+++ Market/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 # comentar Whitenoise en desarrollo
-# from whitenoise.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
